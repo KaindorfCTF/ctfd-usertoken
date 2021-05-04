279e4b956c75efe486c914dff363e5a197636f6f
h4ckd0tm3 <marcel@schnideritsch.at>
2021-05-04 10:45:30 +0200

    Fix imports

--- usertoken.py
+++ usertoken.py
@@ -1,36 +1,27 @@
 import uuid
-import base64
 
-import requests
-from flask import Blueprint
-from flask import current_app as app
-from flask import redirect, render_template, request, session, url_for
-from itsdangerous.exc import BadSignature, BadTimeSignature, SignatureExpired
-
+from CTFd.models import UserFieldEntries, UserFields
 from CTFd.models import (
     Users,
     UserTokens,
     db,
 )
-
-from CTFd.cache import clear_team_session, clear_user_session
-from CTFd.models import Teams, UserFieldEntries, UserFields, Users, db
-from CTFd.utils import config, email, get_app_config, get_config
+from CTFd.utils import config, get_config
+from CTFd.utils import email
 from CTFd.utils import user as current_user
 from CTFd.utils import validators
 from CTFd.utils.config import is_teams_mode
-from CTFd.utils.config.integrations import mlc_registration
-from CTFd.utils.config.visibility import registration_visible
-from CTFd.utils.crypto import verify_password
+from CTFd.utils.decorators import authed_only
 from CTFd.utils.decorators import ratelimit
 from CTFd.utils.decorators.visibility import check_registration_visibility
-from CTFd.utils.helpers import error_for, get_errors, markup
+from CTFd.utils.helpers import get_errors, get_infos, markup
 from CTFd.utils.logging import log
-from CTFd.utils.modes import TEAMS_MODE
-from CTFd.utils.security.auth import login_user, logout_user
-from CTFd.utils.security.signing import unserialize
+from CTFd.utils.security.auth import login_user
+from CTFd.utils.user import get_current_user
 from CTFd.utils.validators import ValidationError
-from CTFd.utils.decorators import authed_only
+from flask import current_app as app
+from flask import redirect, render_template, request, url_for
+
 
 @check_registration_visibility
 @ratelimit(method="POST", limit=10, interval=5)
